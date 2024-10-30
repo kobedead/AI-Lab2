9fab664adc7a7992a856e5a7546fa3a14111facc
kobedead <kobe.frateur@gmail.com>
2024-10-30 17:58:01 +0100

    .

---
+++ run_tensors.py
@@ -0,0 +1,108 @@
+import torch
+import torchvision
+import zipfile
+
+import matplotlib.pyplot as plt
+
+
+def plot_tensor(to_plot: torch.Tensor, title: str):
+    gray_image_tensor = to_plot.view([1, -1, 1])
+    numpy_im = gray_image_tensor.numpy()
+    plt.imshow(numpy_im, cmap=plt.get_cmap("GnBu"), interpolation="none", vmin=0, vmax=1)
+    plt.title(title)
+    plt.show()
+
+def plot_rgb_tensor(to_plot: torch.Tensor, title: str):
+    fig = plt.figure()
+    plt.imshow(torchvision.transforms.ToPILImage()(to_plot), interpolation="None")
+    plt.title(title)
+    plt.show()
+
+def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    ## return mean squared error of 2 tensors
+
+    if input_tensor.size() == target.size() :        ##maybe chack for broadcastable??
+        error_tensor = input_tensor-target
+        mean = torch.mean(error_tensor**2)
+        return mean
+
+    return 0
+
+def create_image() -> torch.Tensor:
+
+    ##tensor -> (color,height, widht)
+                #(color(rgb->3) , 2 , 3) => (3,2,3)
+
+
+    image = torch.ones((3,2,3))
+
+    data = [[0.5021, 0.1138, 0.9047] ,[0.2843, 0.0684, 0.6829] ,[0.1935, 0.5483, 0.3117],
+            [0.8017, 0.8733, 0.6258] ,[0.5914, 0.6004,0.2893], [0.7038, 0.5983, 0.9914]]
+
+    #'distribute' colorvalue through tensor
+    for i in range(len(data[0])) : #3
+        t=0
+        for j in range(len(data)) : #6
+            if j < 3:
+                image[i][0][j] = data[j][i]
+            else :
+                image[i][1][t] = data[j][i]
+                t=t+1
+
+
+    return image
+
+
+def lin_layer_forward(weights: torch.Tensor, random_image: torch.Tensor) -> torch.Tensor:
+
+    output = torch.sum(weights*random_image)
+
+    return output
+
+
+def tensor_network():
+    target = torch.FloatTensor([0.5])
+    print(f"The target is: {target.item():.2f}")
+    plot_tensor(target, "Target")
+
+    input_tensor = torch.FloatTensor([0.4, 0.8, 0.5, 0.3])
+    weights = torch.FloatTensor([0.1, -0.5, 0.9, -1])
+    """START TODO:  Ensure that the tensor 'weights' saves the computational graph and the gradients after backprop"""
+    
+    """END TODO"""
+
+    # remember the activation a of a unit is calculated as follows:
+    #      T
+    # a = W * x, with W the weights and x the inputs of that unit
+    output = lin_layer_forward(weights, input_tensor)
+    print(f"Output value: {output.item(): .2f}")
+    plot_tensor(output.detach(), "Initial Output")
+
+    # We want a measure of how close we are according to our target
+    loss = mse(output, target)
+    print(f"The initial loss is: {loss.item():.2f}\n")
+
+    # Lets update the weights now using our loss..
+    print(f"The current weights are: {weights}")
+
+    """START TODO: the loss needs to be backpropagated"""
+    
+    """END TODO"""
+
+    print(f"The gradients are: {weights.grad}")
+    """START TODO: implement the update step with a learning rate of 0.5"""
+    
+    """END TODO"""
+    print(f"The new weights are: {weights}\n")
+
+    # What happens if we forward through our layer again?
+    output = lin_layer_forward(weights, input_tensor)
+    print(f"Output value: {output.item(): .2f}")
+    plot_tensor(output.detach(), "Improved Output")
+
+
+if __name__ == "__main__":
+
+
+
+    tensor_network()
---
+++ test_tensors.py
@@ -0,0 +1,82 @@
+import torch.nn.functional
+
+import run_tensors as run_tensors
+from run_tensors import mse, plot_rgb_tensor
+
+
+class Options:
+    def __init__(self):
+        # runtime related
+        self.random_seed = 1
+        self.device = "cpu"
+
+        # model related
+        self.save_path = "./models/"
+        self.load_path = "./models/"
+        self.model_name = "lin_reg.pth"
+
+def test_mse():
+    test_input = torch.randn([3, 50, 100])
+    test_target = torch.randn([3, 50, 100])
+    error = mse(test_input, test_target)
+    if error == "NOT IMPLEMENTED":
+        print("MSE not yet implemented... (0/1)")
+    elif error.shape != torch.Size([]):
+        print("MSE not correctly implemented... (0/1)")
+        print("The loss shape is incorrect.")
+        print(f"Yours: {error.shape}")
+        print(f"Correct: {torch.Size([])}")
+    else:
+        error2 = torch.nn.functional.mse_loss(test_input, test_target)
+        if error.item() == error2.item():
+            print("MSE implemented correctly! (1/1)")
+        else:
+            print("MSE not correctly implemented... (0/1)")
+            print("Hint: did you forget to take the mean?")
+
+
+def test_image_matrix():
+    created_image = run_tensors.create_image()
+    if created_image == "NOT IMPLEMENTED":
+        print("create_image not yet implemented... (0/1)")
+    else:
+        good_image = torch.load("images/create_image_tensor.pt")
+        plot_rgb_tensor(good_image, "Target")
+        plot_rgb_tensor(created_image, "Yours")
+        if torch.equal(good_image, created_image):
+            print("create_image implemented correctly! (1/1)")
+        else:
+            print("create_image not implemented correctly... (0/1)")
+            print(f"Your image shape: {created_image.shape}\nOur image shape: {good_image.shape}")
+            print(created_image)
+            print(good_image)
+            print(f"If the shapes match, perhaps there is an error in the pixel values...")
+
+
+def test_tensor_forward():
+    input_tensor = torch.FloatTensor([0.2, 0.1, 0.5, 0.9], device=options.device)
+    weights = torch.FloatTensor([0.4, -0.1, 0.4, -0.5], device=options.device)
+    output = run_tensors.lin_layer_forward(weights, input_tensor)
+    if output == "NOT IMPLEMENTED":
+        print("tensor_forward not yet implemented... (0/1)")
+    elif output.shape != torch.Size([]):
+        print("Something is wrong with the output shape... (0/1)")
+        print(f"Yours: {output.shape}")
+        print(f"Target: {torch.Size([])}")
+    else:
+        if output.item() - (-0.18) < 0.000001:
+            print("tensor_forward implemented correctly! (1/1)")
+        else:
+            print("tensor_forward not implemented correctly... (0/1)")
+
+
+
+
+if __name__ == "__main__":
+    options = Options()
+    print("\nQUESTION 1:\n")
+    test_mse()
+    print("\nQUESTION 2:\n")
+    test_image_matrix()
+    print("\nQUESTION 3:\n")
+    test_tensor_forward()
